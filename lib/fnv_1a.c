3b5aab07c0b8b86bc2e34a8f4937d3089e046bfb
Lars Eggert <lars@eggert.org>
2016-10-06 15:01:10 +0200

    Clean up the FNV-1a code, and add doxygen documentation

--- lib/fnv_1a.c
+++ lib/fnv_1a.c
@@ -1,21 +1,28 @@
-#include <stdint.h>
+#include <assert.h>
 
 #include "fnv_1a.h"
-#include "pkt.h"
 
-
-uint128_t fnv_1a(const struct q_pkt * const pkt,
-                 const uint16_t             skip,
-                 const uint16_t             skip_len)
+uint128_t fnv_1a(const void * restrict const buf,
+                 const size_t                len,
+                 const size_t                skip_pos,
+                 const size_t                skip_len)
 {
+    assert((skip_pos <= len) && (skip_pos + skip_len <= len));
+
     static const uint128_t prime =
         (((uint128_t)0x0000000001000000) << 64) | 0x000000000000013B;
     uint128_t hash =
         (((uint128_t)0x6C62272E07BB0142) << 64) | 0x62B821756295C58D;
-    for (uint16_t i = 0; i < pkt->len; i++)
-        if (i < skip || i >= skip + skip_len) {
-            hash ^= pkt->buf[i];
-            hash *= prime;
-        }
+
+    // two consecutive loops should be faster than one loop with an "if"
+    const uint8_t * restrict const bytes = buf;
+    for (size_t i = 0; i < skip_pos; i++) {
+        hash ^= bytes[i];
+        hash *= prime;
+    }
+    for (size_t i = skip_pos + skip_len; i < len; i++) {
+        hash ^= bytes[i];
+        hash *= prime;
+    }
     return hash;
 }
--- lib/fnv_1a.h
+++ lib/fnv_1a.h
@@ -1,7 +1,31 @@
 #pragma once
 
-#include "pkt.h"
+#include <stddef.h>
+#include <stdint.h>
 
-uint128_t fnv_1a(const struct q_pkt * const pkt,
-                 const uint16_t             skip,
-                 const uint16_t             skip_len);
+
+#ifndef _UINT128_T
+/// If @p stdint.h doesn't define @p uint128_t, define it for ourselves.
+///
+typedef unsigned __int128 uint128_t;
+#endif
+
+///
+/// Compute an [FNV-1a 128-bit
+/// hash](http://www.isthe.com/chongo/tech/comp/fnv/index.html) over the given
+/// buffer. A region of the buffer can be excluded from the hash, by specifying
+/// its starting position in @p skip_pos and its length in @p skip_len.
+///
+/// @param      buf       The buffer.
+/// @param      len       The length of @p buf.
+/// @param      skip_pos  The beginning of the region of @p buf to exclude from
+///                       the hash. Can be 0.
+/// @param      skip_len  The length of the region of @p buf to exclude from the
+///                       hash. Can be 0.
+///
+/// @return     The FNV-1a 128-bit hash of @p buffer, excluding the skip region.
+///
+uint128_t fnv_1a(const void * restrict const buf,
+                 const size_t                len,
+                 const size_t                skip_pos,
+                 const size_t                skip_len);
