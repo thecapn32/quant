b7e845d46aa7e0af771875d3f86439c3955ac209
Lars Eggert <lars@eggert.org>
2017-05-16 16:15:35 +0200

    Also use a timeout for the server

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-ninja && tmux new-session -d bin/server -t0 \; \
-        split-window -h "sleep 0.1; bin/client " \; \
+ninja && tmux new-session -d bin/server -t10 \; \
+        split-window -h "sleep 0.1; bin/client -t5" \; \
         set remain-on-exit on \; \
         attach
