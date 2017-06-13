f030e268539845642337e69c13ced7f41d8fddd7
Lars Eggert <lars@eggert.org>
2017-06-13 10:54:21 +0200

    Take optional argument controlling verbosity

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env bash
 
 rm -f /cores/server.core /cores/client.core
-ninja && tmux new-session -d "sleep 0.1; bin/client -t3" \; \
-        split-window -h bin/server -t5 \; \
+
+v=${1:-5}
+
+ninja && tmux new-session -d "sleep 0.1; bin/client -t3 -v$v" \; \
+        split-window -h "bin/server -t5 -v$v" \; \
         set remain-on-exit on \; \
         attach
