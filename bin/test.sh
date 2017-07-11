bbf4c395ac64db3b324390a29fea4997a1e54d9f
Lars Eggert <lars@eggert.org>
2017-07-11 16:03:50 +0200

    Use iTerm2 integration of tmux

--- bin/test.sh
+++ bin/test.sh
@@ -4,7 +4,7 @@ rm -f /cores/server.core /cores/client.core
 
 v=${1:-5}
 
-ninja && tmux new-session -d "sleep 0.1; bin/client -v$v" \; \
+ninja && tmux -CC \
+        new-session "sleep 0.1; bin/client -v$v" \; \
         split-window -h "bin/server -v$v" \; \
-        set remain-on-exit on \; \
-        attach
+        set remain-on-exit on
