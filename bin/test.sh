f02421aa1c3dc5c924311cafce75c2a10470ffc6
Lars Eggert <lars@eggert.org>
2017-08-20 17:54:28 +0200

    Use dummynet to add some artificial delay and enforce a bandwidth cap

--- bin/test.sh
+++ bin/test.sh
@@ -4,12 +4,15 @@ rm -f /cores/*.core
 
 set -e
 
+# by default, test quant
 c=${1:-quant}
 s=${2:-quant}
 
+# (re-)build the client (and possibly server) to test
 ninja "$c"
 [ "$c" != "$s" ] && ninja "$s"
 
+# commands to run the different clients against 127.0.0.1:4433
 case $c in
         quant)
                 c=bin/client
@@ -32,6 +35,7 @@ case $c in
                 ;;
 esac
 
+# commands to run the different servers on  127.0.0.1:4433
 case $s in
         quant)
                 s=bin/server
@@ -55,9 +59,25 @@ case $s in
                 ;;
 esac
 
+# if we are on MacOS X, configure the firewall to add delay and loss
+if [ -x /usr/sbin/dnctl ]; then
+        # create pipes to limit bandwidth and add loss
+        sudo dnctl pipe 1 config bw 64Kbit/s delay 50 queue 10Kbytes #plr 0.25
+        sudo dnctl pipe 2 config bw 64Kbit/s delay 50 queue 10Kbytes #plr 0.25
+        sudo pfctl -f - <<EOF
+                dummynet out proto udp from any to 127.0.0.1 port 4433 pipe 1
+                dummynet out proto udp from 127.0.0.1 port 4433 to any pipe 2
+EOF
+        sudo pfctl -e || true
+fi
+
 tmux -CC \
         new-session "sleep 0.1; $c" \; \
         split-window -h "$s" \; \
         set remain-on-exit on
 
-ls -lta /cores
+# if we are on MacOS X, unconfigure the firewall
+if [ -x /usr/sbin/dnctl ]; then
+        sudo pfctl -d
+        sudo dnctl -f flush
+fi
