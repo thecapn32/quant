c16975f6a597473730468d1181d1da35a31e2a33
Lars Eggert <lars@eggert.org>
2017-08-31 10:04:03 +0200

    Use dummynet for traffic shaping and loss generation

--- bin/test.sh
+++ bin/test.sh
@@ -8,17 +8,20 @@ set -e
 c=${1:-quant}
 s=${2:-quant}
 
+# port to run servers on
+port=44333
+
 # (re-)build the client (and possibly server) to test
 ninja "$c"
 [ "$c" != "$s" ] && ninja "$s"
 
-# commands to run the different clients against 127.0.0.1:4433
+# commands to run the different clients against 127.0.0.1:$port
 case $c in
         quant)
-                c=bin/client
+                c="bin/client -p $port"
                 ;;
         quicly)
-                c="external/usr/local/bin/cli 127.0.0.1 4433"
+                c="external/usr/local/bin/cli 127.0.0.1 $port"
                 ;;
         minq)
                 c="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
@@ -26,7 +29,7 @@ case $c in
                 ;;
         ngtcp2)
                 c="external/ngtcp2-prefix/src/ngtcp2/examples/client \
-                        127.0.0.1 4433"
+                        127.0.0.1 $port"
                 ;;
         mozquic)
                 c="env MOZQUIC_NSS_CONFIG=external/mozquic-prefix/src/mozquic/sample/nss-config \
@@ -35,14 +38,14 @@ case $c in
                 ;;
 esac
 
-# commands to run the different servers on  127.0.0.1:4433
+# commands to run the different servers on  127.0.0.1:$port
 case $s in
         quant)
-                s=bin/server
+                s="bin/server -p $port"
                 ;;
         quicly)
-                s="external/usr/local/bin/cli -k lib/src/key.pem -c \
-                        lib/src/cert.pem 127.0.0.1 4433"
+                s="external/usr/local/bin/cli -v -k lib/src/key.pem -c \
+                        lib/src/cert.pem 127.0.0.1 $port"
                 ;;
         minq)
                 s="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
@@ -50,7 +53,7 @@ case $s in
                 ;;
         ngtcp2)
                 s="external/ngtcp2-prefix/src/ngtcp2/examples/server \
-                        127.0.0.1 4433 lib/src/key.pem lib/src/cert.pem"
+                        127.0.0.1 $port lib/src/key.pem lib/src/cert.pem"
                 ;;
         mozquic)
                 s="env MOZQUIC_NSS_CONFIG=external/mozquic-prefix/src/mozquic/sample/nss-config \
@@ -62,13 +65,13 @@ esac
 # if we are on MacOS X, configure the firewall to add delay and loss
 if [ -x /usr/sbin/dnctl ]; then
         # create pipes to limit bandwidth and add loss
-        sudo dnctl pipe 1 config bw 64Kbit/s delay 50 queue 10Kbytes #plr 0.25
-        sudo dnctl pipe 2 config bw 64Kbit/s delay 50 queue 10Kbytes #plr 0.25
+        sudo dnctl pipe 1 config bw 64Kbit/s #delay 50 queue 10Kbytes #plr 0.25
+        sudo dnctl pipe 2 config bw 64Kbit/s #delay 50 queue 10Kbytes #plr 0.25
         sudo pfctl -f - <<EOF
-                dummynet out proto udp from any to 127.0.0.1 port 4433 pipe 1
-                dummynet out proto udp from 127.0.0.1 port 4433 to any pipe 2
+                dummynet out proto udp from any to 127.0.0.1 port $port pipe 1
+                dummynet out proto udp from 127.0.0.1 port $port to any pipe 2
 EOF
-        sudo pfctl -e || true
+        # sudo pfctl -e || true
 fi
 
 tmux -CC \
@@ -78,6 +81,6 @@ tmux -CC \
 
 # if we are on MacOS X, unconfigure the firewall
 if [ -x /usr/sbin/dnctl ]; then
-        sudo pfctl -d
+        # sudo pfctl -d
         sudo dnctl -f flush
 fi
