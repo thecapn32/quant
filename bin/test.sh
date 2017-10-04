ffac52d08262716bf6d858db7b9358ce76dbb042
Lars Eggert <lars@eggert.org>
2017-10-04 10:25:00 -0700

    Fix minq

--- bin/test.sh
+++ bin/test.sh
@@ -10,42 +10,44 @@ s=${2:-quant}
 
 # port to run servers on
 port=44333
+addr=127.0.0.1
 
 # (re-)build the client (and possibly server) to test
 ninja "$c"
 [ "$c" != "$s" ] && ninja "$s"
 
-# commands to run the different clients against 127.0.0.1:$port
+# commands to run the different clients against $addr:$port
 case $c in
         quant)
-                c="bin/client https://127.0.0.1:$port/"
+                c="bin/client https://$addr:$port/"
                 ;;
         quicly)
-                c="external/usr/local/bin/cli -vv -p / 127.0.0.1 $port"
+                c="external/usr/local/bin/cli -vv -p / $addr $port"
                 ;;
         minq)
                 c="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
-                        external/go/src/github.com/ekr/minq/bin/client/main.go"
+                        external/go/src/github.com/ekr/minq/bin/client/main.go \
+                        -addr $addr:$port -http /"
                 ;;
         ngtcp2)
                 c="external/ngtcp2-prefix/src/ngtcp2/examples/client \
-                        127.0.0.1 $port"
+                        $addr $port"
                 ;;
         mozquic)
                 c="env MOZQUIC_NSS_CONFIG=external/mozquic-prefix/src/mozquic/sample/nss-config \
                         DYLD_LIBRARY_PATH=external/nss-prefix/src/dist/$(cat external/nss-prefix/src/dist/latest)/lib \
-                        external/mozquic-prefix/src/mozquic/client -peer 127.0.0.1"
+                        external/mozquic-prefix/src/mozquic/client -peer $addr"
                 ;;
 esac
 
-# commands to run the different servers on  127.0.0.1:$port
+# commands to run the different servers on  $addr:$port
 case $s in
         quant)
                 s="bin/server -p $port -d .."
                 ;;
         quicly)
                 s="external/usr/local/bin/cli -vv -k lib/src/key.pem -c \
-                        lib/src/cert.pem 127.0.0.1 $port"
+                        lib/src/cert.pem $addr $port"
                 ;;
         minq)
                 s="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
@@ -53,7 +55,7 @@ case $s in
                 ;;
         ngtcp2)
                 s="external/ngtcp2-prefix/src/ngtcp2/examples/server \
-                        127.0.0.1 $port lib/src/key.pem lib/src/cert.pem"
+                        $addr $port lib/src/key.pem lib/src/cert.pem"
                 ;;
         mozquic)
                 s="env MOZQUIC_NSS_CONFIG=external/mozquic-prefix/src/mozquic/sample/nss-config \
@@ -68,8 +70,8 @@ if [ -x /usr/sbin/dnctl ]; then
         sudo dnctl pipe 1 config bw 64Kbit/s #delay 50 queue 10Kbytes #plr 0.25
         sudo dnctl pipe 2 config bw 64Kbit/s #delay 50 queue 10Kbytes #plr 0.25
         sudo pfctl -f - <<EOF
-                dummynet out proto udp from any to 127.0.0.1 port $port pipe 1
-                dummynet out proto udp from 127.0.0.1 port $port to any pipe 2
+                dummynet out proto udp from any to $addr port $port pipe 1
+                dummynet out proto udp from $addr port $port to any pipe 2
 EOF
         # sudo pfctl -e || true
 fi
