712205191de8c0c3a48acfb26864e23ab57c2320
Lars Eggert <lars@eggert.org>
2017-11-20 12:42:57 +0100

    Test cases against warpcore backend

--- bin/test.sh
+++ bin/test.sh
@@ -13,12 +13,22 @@ addr=127.0.0.1
 port=4433 # mozquic server can only run on 4433 at the moment
 path=/ #index.html
 dir=/Users/lars/Sites/lars/output
-cert=lib/src/quant.eggert.org/fullchain.pem
-key=lib/src/quant.eggert.org/privkey.pem
+cert=/etc/letsencrypt/live/slate.eggert.org/fullchain.pem
+key=/etc/letsencrypt/live/slate.eggert.org/privkey.pem
 
 # (re-)build the client (and possibly server) to test
-ninja "$c"
-[ "$c" != "$s" ] && ninja "$s"
+if [ "$c" == wcquant ] || [ "$s" == wcquant ] ||
+   [ "$c" == wsquant ] || [ "$s" == wsquant ]; then
+        delay=0.1
+        vagrant ssh -c "\
+                mkdir -p /vagrant/Linux; \
+                cd /vagrant/Linux; \
+                cmake -GNinja .. && ninja"
+else
+        delay=0.1
+        ninja "$c"
+        [ "$c" != "$s" ] && ninja "$s"
+fi
 
 export ASAN_OPTIONS=strict_string_checks=1:strict_init_order=1:detect_stack_use_after_return=1:detect_leaks=1:check_initialization_order=1:sleep_before_dying=30:alloc_dealloc_mismatch=1:detect_invalid_pointer_pairs=1
 export UBSAN_OPTIONS=suppressions=../misc/ubsan.supp:print_stacktrace=1
@@ -26,31 +36,39 @@ export UBSAN_OPTIONS=suppressions=../misc/ubsan.supp:print_stacktrace=1
 # commands to run the different clients against $addr:$port
 case $c in
         quant)
-                c="bin/client -v5 https://$addr:$port$path"
+                cc="bin/client -v5 https://$addr:$port$path"
+                ;;
+        wsquant)
+                cc="bin/client -i vboxnet3 -v5 https://172.28.128.3:$port$path"
+                ;;
+        wcquant)
+                cc="vagrant ssh -c \"\
+                        /vagrant/Linux/bin/client -i enp0s8 -v5 \
+                                https://172.28.128.1:$port$path\""
                 ;;
         quicly)
-                c="external/usr/local/bin/cli -l /tmp/quicly-c.log -v \
+                cc="external/usr/local/bin/cli -l /tmp/quicly-c.log -v \
                         -p $path $addr $port"
                 ;;
         minq)
-                c="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
+                cc="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
                         external/go/src/github.com/ekr/minq/bin/client/main.go \
                         -addr $addr:$port -http $path  2>&1 | \
                                 grep -v -E 'Frame type (byte )?0'"
                 ;;
         ngtcp2)
-                c="echo GET / | external/ngtcp2-prefix/src/ngtcp2/examples/client \
+                cc="echo GET / | external/ngtcp2-prefix/src/ngtcp2/examples/client \
                         -i $addr $port"
                 ;;
         mozquic)
-                c="env MOZQUIC_LOG=all:9 \
+                cc="env MOZQUIC_LOG=all:9 \
                         MOZQUIC_NSS_CONFIG=external/mozquic-prefix/src/mozquic/sample/nss-config \
                         DYLD_LIBRARY_PATH=external/mozquic-prefix/src/dist/$(cat external/mozquic-prefix/src/dist/latest)/lib \
                         external/mozquic-prefix/src/mozquic/client \
                                 -peer $addr:$port -get $path -send-close"
                 ;;
         picoquic)
-                c="external/picoquic-prefix/src/picoquic/picoquicdemo \
+                cc="external/picoquic-prefix/src/picoquic/picoquicdemo \
                         $addr $port -r"
                 ;;
 esac
@@ -58,40 +76,50 @@ esac
 # commands to run the different servers on  $addr:$port
 case $s in
         quant)
-                s="sudo bin/server -v5 -p $port -d $dir"
+                sc="bin/server -v5 -p $port -d $dir"
+                ;;
+        wsquant)
+                sc="vagrant ssh -c \"\
+                        /vagrant/Linux/bin/server -i enp0s8 -v5 -p $port \
+                                -c ~/slate.eggert.org/fullchain.pem \
+                                -k ~/slate.eggert.org/privkey.pem \
+                                -d /usr/share/apache2/default-site\""
+                ;;
+        wcquant)
+                sc="bin/server -v5 -i vboxnet3 -p $port -d $dir"
                 ;;
         quicly)
-                s="external/usr/local/bin/cli -a hq-05 -l /tmp/quicly-s.log -v \
+                sc="external/usr/local/bin/cli -a hq-05 -l /tmp/quicly-s.log -v \
                         -k $key -c $cert $addr $port"
                 ;;
         minq)
-                s="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
+                sc="env MINQ_LOG=\* GOPATH=$(pwd)/external/go go run \
                         external/go/src/github.com/ekr/minq/bin/server/main.go \
                         -addr $addr:$port -http -key $key \
                         -cert $cert -server-name $addr 2>&1 \
                         | grep -v -E 'Frame type (byte )?0'"
                 ;;
         ngtcp2)
-                s="external/ngtcp2-prefix/src/ngtcp2/examples/server \
+                sc="external/ngtcp2-prefix/src/ngtcp2/examples/server \
                         -d $dir $addr $port $key $cert"
                 ;;
         mozquic)
-                s="env MOZQUIC_LOG=all:9 \
+                sc="env MOZQUIC_LOG=all:9 \
                         MOZQUIC_NSS_CONFIG=external/mozquic-prefix/src/mozquic/sample/nss-config \
                         DYLD_LIBRARY_PATH=external/mozquic-prefix/src/dist/$(cat external/mozquic-prefix/src/dist/latest)/lib \
                         external/mozquic-prefix/src/mozquic/server -send-close"
                 ;;
         picoquic)
-                s="external/picoquic-prefix/src/picoquic/picoquicdemo \
+                sc="external/picoquic-prefix/src/picoquic/picoquicdemo \
                         -p $port -k $key -c $cert"
                 ;;
 esac
 
-# if we are on MacOS X, configure the firewall to add delay and loss
+# # if we are on MacOS X, configure the firewall to add delay and loss
 # if [ -x /usr/sbin/dnctl ]; then
 #         # create pipes to limit bandwidth and add loss
-#         sudo dnctl pipe 1 config plr 0.25 #bw 64Kbit/s delay 250 queue 10Kbytes #plr 0.5
-#         sudo dnctl pipe 2 config plr 0 #bw 64Kbit/s delay 250 queue 10Kbytes #plr 0.25
+#         sudo dnctl pipe 1 config bw 64Kbit/s delay 250 queue 10Kbytes #plr 0.5
+#         sudo dnctl pipe 2 config bw 64Kbit/s delay 250 queue 10Kbytes #plr 0.25
 #         sudo pfctl -f - <<EOF
 #                 dummynet out proto udp from any to $addr port $port pipe 1
 #                 dummynet out proto udp from $addr port $port to any pipe 2
@@ -100,8 +128,8 @@ esac
 # fi
 
 tmux -CC \
-        new-session "sleep 0.1; $c" \; \
-        split-window -h "$s" \; \
+        new-session "sleep $delay; $cc" \; \
+        split-window -h "$sc" \; \
         set remain-on-exit on
 
 # if we are on MacOS X, unconfigure the firewall
