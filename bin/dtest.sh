387508f328be9a46e84e18949ee69c0530beb4a2
Lars Eggert <lars@eggert.org>
2018-02-08 22:04:39 +0100

    Fixes

--- bin/dtest.sh
+++ bin/dtest.sh
@@ -7,9 +7,9 @@ set -e
 docker-compose up --no-color --force-recreate --remove-orphans -d
 
 tmux -CC \
-        new-session "docker-compose logs -f client | cut -f2 -d\|" \; \
-        split-window -h "docker-compose logs -f switch | cut -f2 -d\|" \; \
-        split-window -h "docker-compose logs -f server | cut -f2 -d\|" \; \
+        new-session "docker-compose logs -f client | cut -f2 -d\\|" \; \
+        split-window -h "docker-compose logs -f valve | cut -f2 -d\\|" \; \
+        split-window -h "docker-compose logs -f server | cut -f2 -d\\|" \; \
         set remain-on-exit on
 
 docker-compose kill
