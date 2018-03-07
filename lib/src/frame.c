4f81b87f0fbe27fd74a98b79ebcf4df7fe0efc78
Lars Eggert <lars@eggert.org>
2018-03-07 13:18:21 +0100

    Fix release build

--- lib/src/frame.c
+++ lib/src/frame.c
@@ -47,10 +47,12 @@
 #include "stream.h"
 
 
+#ifndef NDEBUG
 #define max_strm_id(s)                                                         \
     (is_set(STRM_FL_INI_SRV, (s)->id) != (s)->c->is_clnt == false              \
          ? (s)->c->tp_local.max_strm_bidi                                      \
          : (s)->c->tp_peer.max_strm_bidi)
+#endif
 
 
 static uint16_t __attribute__((nonnull))
