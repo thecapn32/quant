2cd0bf04ae4e67c62aac40d4f395471641df8762
Lars Eggert <lars@eggert.org>
2018-03-08 15:54:48 +0100

    Really fix release build

--- lib/src/tls.c
+++ lib/src/tls.c
@@ -545,7 +545,7 @@ static int encrypt_ticket_cb(ptls_encrypt_ticket_t * self
                              ptls_buffer_t * dst,
                              ptls_iovec_t src)
 {
-#ifnde NDEBUG
+#ifndef NDEBUG
     struct q_conn * const c = *ptls_get_data_ptr(tls);
 #endif
     uint64_t tid;
