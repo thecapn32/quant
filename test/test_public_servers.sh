42a2d16c42af38eaf9fe0e35062c376429cd8df1
Lars Eggert <lars@eggert.org>
2018-03-09 13:16:25 +0100

    Report results as wdiff to expected state

--- test/test_public_servers.sh
+++ test/test_public_servers.sh
@@ -43,7 +43,7 @@ declare -A servers=(
 )
 
 results=(live vneg hshk data close zrtt hrr)
-declare -A live vneg hshk data close zrtt hrr
+declare -A ${results[@]}
 
 
 iface=$(route get default | grep interface: | cut -f2 -d: | tr -d "[:space:]")
@@ -55,6 +55,14 @@ else
         sed=$(which sed)
 fi
 
+# use colordiff, if installed
+if which colordiff > /dev/null; then
+        colordiff=$(which colordiff)
+else
+        colordiff=$(which cat)
+fi
+
+
 pid=$$
 script=$(basename -s .sh $0)
 
@@ -63,23 +71,22 @@ function test_server {
         # run quant client and produce a pure ASCII log for post-processing
         local cache="/tmp/$script.$1.$pid.cache"
         local opts="-i $iface -t3 -v4 -s $cache"
+        local sed_pattern='s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g'
+        local log_base="/tmp/$script.$1.$pid"
         rm -f "$cache"
 
         # initial 1rtt run
         bin/client $opts "https://${servers[$1]}:4433/index.html" 2>&1 | \
-                $sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' \
-                > "/tmp/$script.$1.$pid.1rtt.log"
+                $sed -r "$sed_pattern" > "$log_base.1rtt.log"
 
         # consecutive 0rtt run
         bin/client $opts "https://${servers[$1]}:4433/index.html" 2>&1 | \
-                $sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' \
-                > "/tmp/$script.$1.$pid.0rtt.log"
+                $sed -r "$sed_pattern" > "$log_base.0rtt.log"
         rm -f "$cache"
 
         # hrr run
         bin/client $opts "https://${servers[$1]}:4434/index.html" 2>&1 | \
-                $sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' \
-                > "/tmp/$script.$1.$pid.hrr.log"
+                $sed -r "$sed_pattern" > "$log_base.hrr.log"
 
         printf "%s " "$s"
 }
@@ -89,26 +96,22 @@ function analyze {
         # analyze 1rtt
         local log="/tmp/$script.$1.$pid.1rtt.log"
         awk '/RX len=/{exit 1}' "$log"
-        if [ $? == 1 ]; then
-                live[$1]="*"
-        fi
+        [ $? == 1 ] && live[$1]="*"
 
         awk '/0xbabababa, retrying with/{exit 1}' "$log"
-        if [ $? == 1 ]; then
-                vneg[$1]=V
-        fi
+        [ $? == 1 ] && vneg[$1]=V
 
         awk '/TX len=.*Short/{x=1}; x && /RX len=.*Short/{exit 1}' "$log"
-        if [ $? == 1 ]; then
-                hshk[$1]=H
-        fi
+        [ $? == 1 ] && hshk[$1]=H
 
         awk '/read .* bytes on clnt conn/{exit 1}' "$log"
-        if [ $? == 1 ]; then
-                data[$1]=D
-        fi
+        [ $? == 1 ] && data[$1]=D
 
-        awk 'BEGIN{t=-1}; /TX len=/{t=1}; /RX len=/{t=0}; /CLOSE err=0x0000/{if (t==1) { tc=1 } else { rc=1 }}; END{exit tc+rc}' "$log"
+        awk '   BEGIN{t=-1};
+                /TX len=/{t=1};
+                /RX len=/{t=0};
+                /CLOSE err=0x0000/{if (t==1) { tc=1 } else { rc=1 }};
+                END{exit tc+rc}' "$log"
         local ret=$?
         if [ $ret == 2 ]; then
                 close[$1]=C
@@ -119,18 +122,15 @@ function analyze {
 
         # analyze 0rtt
         local log="/tmp/$script.$1.$pid.0rtt.log"
-        awk '/connected after 0-RTT/{x=1}; x && /CLOSE err=0x0000/{exit 1}' "$log"
-        if [ $? == 1 ]; then
-                zrtt[$1]=Z
-        fi
+        awk '   /connected after 0-RTT/{x=1};
+                x && /CLOSE err=0x0000/{exit 1}' "$log"
+        [ $? == 1 ] && zrtt[$1]=Z
         rm -f "$log"
 
         # analyze hrr
         local log="/tmp/$script.$1.$pid.hrr.log"
         awk '/RX len=.*Retry/{x=1}; x && /CLOSE err=0x0000/{exit 1}' "$log"
-        if [ $? == 1 ]; then
-                hrr[$1]=S
-        fi
+        [ $? == 1 ] && hrr[$1]=S
         rm -f "$log"
 }
 
@@ -142,18 +142,22 @@ done
 wait
 printf "\\n\\n"
 
-printf "%10s\\t" ""
+tmp=$(mktemp)
+printf "%10s\\t" "" >> "$tmp"
 for r in "${results[@]}"; do
-        printf "%s\\t" "$r"
+        printf "%s\\t" "$r" >> "$tmp"
 done
-printf "\\n"
+printf "\\n" >> "$tmp"
 
 for s in "${!servers[@]}"; do
-        printf "%-10s\\t" "$s"
+        printf "%-10s\\t" "$s" >> "$tmp"
         analyze "$s"
         for r in "${results[@]}"; do
-                tmp="$r[$s]"
-                printf "%s\\t" "${!tmp}"
+                v="$r[$s]"
+                printf "%s\\t" "${!v}" >> "$tmp"
         done
-        printf "\\n"
+        printf "\\n" >> "$tmp"
 done
+
+wdiff -n "$(dirname $0)/$script.result" "$tmp" | $colordiff
+rm -f "$tmp"
