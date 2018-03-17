2edf4653691d83ebe0514e773dff97022e97c89b
Lars Eggert <lars@eggert.org>
2018-03-17 12:49:12 +0000

    Fix 0rtt

--- lib/src/conn.c
+++ lib/src/conn.c
@@ -200,7 +200,7 @@ static uint32_t __attribute__((nonnull(1))) tx_stream(struct q_stream * const s,
             continue;
         }
 
-        if (s->c->state >= CONN_STAT_ESTB && !rtx && meta(v).tx_len != 0) {
+        if (s->c->state >= CONN_STAT_HSHK_DONE && !rtx && meta(v).tx_len != 0) {
             warn(DBG,
                  "skipping %s pkt " FMT_PNR_OUT " on str " FMT_SID " during %s",
                  meta(v).tx_len ? "already-tx'ed" : "fresh", meta(v).nr, s->id,
