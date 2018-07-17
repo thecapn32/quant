36418adf4183dac56ebce19b2068ff93b0e2b354
Lars Eggert <lars@eggert.org>
2018-07-17 15:02:32 +0200

    Compute some stats. Colorize output.

--- bin/dtest.sh
+++ bin/dtest.sh
@@ -4,12 +4,22 @@ rm -f /cores/*.core
 
 set -e
 
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+# blue=$(tput setaf 4)
+bold=$(tput bold)
+norm=$(tput sgr0)
+
 # clean up from previous run
 docker-compose down --timeout 1 --remove-orphans 2> /dev/null
 
 # if arguments are given, assume they are tests (otherwise run all)
 tests="qvalve-tests/*"
 [ ! -z "$*" ] && tests=$*
+
+declare -A status col
+col[ok]="${bold}${green}"
+col[fail]="${bold}${red}"
 
 for t in $tests; do
         echo -n "$t"
@@ -31,18 +41,27 @@ for t in $tests; do
         done
 
         if [ "${ret[client]}" == 0 ] && [ "${ret[server]}" == 0 ]; then
-                echo " - success"
+                stat=ok
         else
-                echo " - failed"
+                stat=fail
                 tmux -CC \
                         new-session "${log[client]}" \; \
                         split-window -h "${log[valve]}" \; \
                         split-window -h "${log[server]}" \; \
                         set remain-on-exit on
         fi
+        echo " - ${col[$stat]}${stat}${norm}"
+        status[$stat]=$((status[$stat] + 1))
+        status[all]=$((status[all] + 1))
 
         docker-compose rm --force --stop > /dev/null 2> /dev/null
 done
+
+for s in ok fail; do
+        [ -n "${status[$s]}" ] && \
+                echo ${col[$s]}$s${norm} ${status[$s]}/${status[all]}
+done
+
 
 docker-compose kill
 docker-compose down --timeout 1 --remove-orphans 2> /dev/null
