829776c264492ab5500c473c30ae6e036ce10043
Lars Eggert <lars@eggert.org>
2018-07-18 10:11:05 +0200

    Parallelize tests

--- bin/dtest.sh
+++ bin/dtest.sh
@@ -9,46 +9,65 @@ green=$(tput setaf 2)
 bold=$(tput bold)
 norm=$(tput sgr0)
 
-# clean up from previous run
-docker-compose down --timeout 1 --remove-orphans 2> /dev/null
+
+declare -A status col=(
+        [ok]="${bold}${green}"
+        [fail]="${bold}${red}"
+)
+
+
+function run_test() {
+        base=$(basename -s .qv "$1")
+        dc="docker-compose -p $base"
+
+        $dc up --no-start 2> /dev/null
+        cmd="$dc run --detach --no-deps -T --service-ports"
+        $cmd --name "$base-server" server > /dev/null &
+        $cmd --name "$base-valve" valve \
+                env PYTHONUNBUFFERED=1 qvalve -ra "$base-server" -r "/$t" \
+                        > /dev/null &
+        $cmd --name "$base-client" client \
+                client -v4 -i eth0 "https://$base-valve/10000" \
+                        > /dev/null &
+        wait
+
+        ret=ok
+        if [ "$(docker container wait "$base-client")" != 0 ] || \
+           [ "$(docker container wait "$base-server")" != 0 ]; then
+                ret=fail
+        fi
+
+        for s in server valve client; do
+                log="$base-$s.log"
+                if [ "$ret" = "ok" ]; then
+                        rm -f "$log"
+                else
+                        docker logs "$base-$s" > "$log" 2>&1
+                fi
+        done
+
+        echo "$t ... ${col[$ret]}${ret}${norm}"
+
+        $dc down --timeout 1 2> /dev/null
+        echo $ret > "/tmp/$$-$base.ret"
+}
+
 
 # if arguments are given, assume they are tests (otherwise run all)
 tests="qvalve-tests/*"
 [ ! -z "$*" ] && tests=$*
 
-declare -A col=(
-        [ok]="${bold}${green}"
-        [fail]="${bold}${red}"
-)
-
-declare -A status
 for t in $tests; do
-        echo -n "$t ..."
+        run_test "$t" &
+done
+wait
 
-        cmd="docker-compose run --detach --no-deps -T --service-ports"
-        $cmd --name server server > /dev/null 2> /dev/null
-        $cmd --name valve valve \
-                env PYTHONUNBUFFERED=1 qvalve -ra server -r "/$t" > /dev/null
-        $cmd --name client client \
-                client -v4 -i eth0 https://valve/10000 > /dev/null
-
-        if [ "$(docker container wait client)" == 0 ] && \
-           [ "$(docker container wait server)" == 0 ]; then
-                stat=ok
-        else
-                stat=fail
-                # save the logs
-                base=$(basename -s .qv "$t")
-                for s in server valve client; do
-                        docker logs $s > "${base}-${s}.log" 2>&1
-                done
-        fi
-
-        echo " ${col[$stat]}${stat}${norm}"
-        status[$stat]=$((status[$stat] + 1))
+for t in $tests; do
+        base=$(basename -s .qv "$t")
+        ret=$(cat "/tmp/$$-$base.ret")
+        rm "/tmp/$$-$base.ret"
+        status[$ret]=$((status[$ret] + 1))
         status[all]=$((status[all] + 1))
-
-        docker-compose rm --force --stop > /dev/null 2> /dev/null
 done
 
 for s in ok fail; do
@@ -56,5 +75,3 @@ for s in ok fail; do
                 echo "${col[$s]}$s${norm} ${status[$s]}/${status[all]}"
 done
 
-docker-compose kill
-docker-compose down --timeout 1 --remove-orphans 2> /dev/null
