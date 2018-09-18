3b43623b3b42862f1aff9b7a45ebb205244581c6
Brian Trammell <brian@trammell.ch>
2018-09-18 09:46:28 -0400

    we already track largest recv

--- lib/src/pkt.c
+++ lib/src/pkt.c
@@ -641,8 +641,7 @@ bool dec_pkt_hdr_remainder(struct w_iov * const v,
         }
     } else {
         // short header, spin the bit
-        if (nr > c->pn_data.lg_recv) {
-            c->pn_data.lg_recv = nr;
+        if (nr == diet_max(&(c->pn_data.pn.recv_all))) {
             if (c->is_clnt) {
                 c->next_spin = ((meta(v).hdr.flags & F_SH_SPIN) == 0);
             } else {
