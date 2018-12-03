04ca26fff29189a202c1a7cf657f419f6d637031
Lars Eggert <lars@eggert.org>
2018-12-03 16:58:49 +0200

    Add implementation note

--- lib/src/quic.h
+++ lib/src/quic.h
@@ -55,6 +55,7 @@ struct cid {
     splay_entry(cid) node_seq;
     splay_entry(cid) node_id;
     uint64_t seq;            ///< Connection ID sequence number
+    /// XXX len must precede id for cid_cmp() to work
     uint8_t len;             ///< Connection ID length
     uint8_t id[MAX_CID_LEN]; ///< Connection ID
     uint8_t srt[SRT_LEN];    ///< Stateless Reset Token
