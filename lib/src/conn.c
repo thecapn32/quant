0ac6245f218db9cd0dc3f2ef581a6b0295f78500
Lars Eggert <lars@eggert.org>
2019-01-25 09:27:57 +0200

    Fix ACKing

--- lib/src/conn.c
+++ lib/src/conn.c
@@ -1135,25 +1135,27 @@ rx(struct ev_loop * const l, ev_io * const rx_w, int _e __attribute__((unused)))
         ev_timer_again(l, &c->idle_alarm);
 
         // is a TX needed for this connection?
+        const bool needed_tx = c->needs_tx;
         if (c->needs_tx)
-            tx(c, 0);
-        else
-            for (epoch_t e = c->epoch_in_before_rx; e <= ep_data; e++) {
-                if (c->cstreams[e] == 0 || e == ep_0rtt)
-                    // we don't ACK in abandoned and 0rtt pn spaces
-                    continue;
-                struct pn_space * const pn = pn_for_epoch(c, e);
-                switch (needs_ack(pn)) {
-                case imm_ack:
-                    tx_ack(c, e);
-                    break;
-                case del_ack:
-                    ev_timer_again(loop, &c->ack_alarm);
-                    break;
-                case no_ack:
-                    break;
-                }
+            tx(c, 0); // this clears c->needs_tx
+
+        for (epoch_t e = c->epoch_in_before_rx; e <= ep_data; e++) {
+            if (c->cstreams[e] == 0 || e == ep_0rtt ||
+                (needed_tx && e == epoch_in(c)))
+                // don't ACK abandoned and 0rtt pn spaces, or if we TX'ed above
+                continue;
+            struct pn_space * const pn = pn_for_epoch(c, e);
+            switch (needs_ack(pn)) {
+            case imm_ack:
+                tx_ack(c, e);
+                break;
+            case del_ack:
+                ev_timer_again(loop, &c->ack_alarm);
+                break;
+            case no_ack:
+                break;
             }
+        }
 
         if (unlikely(c->tx_rtry))
             // if we sent a retry, forget the entire connection existed
