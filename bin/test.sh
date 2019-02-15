104e490f254745fbda3531d5339015ccf18e42e6
Lars Eggert <lars@eggert.org>
2019-02-15 10:41:20 +0200

    Make the server always listen on the default ports

--- bin/test.sh
+++ bin/test.sh
@@ -107,7 +107,8 @@ esac
 # commands to run the different servers on  $addr:$port
 case $s in
         quant)
-                sc="bin/server -v5 -c $cert -k $key -i $iface -p $port -d $dir"
+                sc="bin/server -v5 -c $cert -k $key -i $iface \
+                    -p 4433 -p 4434 -p $port -d $dir"
                 ;;
         wquant)
                 sc="vagrant ssh -c \"\
