26d80d8ab74ccbf4f30f268710c36eba9066f844
Lars Eggert <lars@eggert.org>
2019-05-17 10:59:53 +0300

    STREAM_DATA_BLOCKED is RTX'ed automatically

--- lib/src/recovery.c
+++ lib/src/recovery.c
@@ -257,6 +257,9 @@ static void __attribute__((nonnull)) on_pkt_lost(struct pkt_meta * const m)
                 case FRM_CID:
                     c->max_cid_seq_out = m->min_cid_seq - 1;
                     break;
+                case FRM_SDB:
+                    // STREAM_DATA_BLOCKED is RTX'ed automatically
+                    break;
                 default:
                     die("unhandled RTX of 0x%02x frame", i);
                 }
