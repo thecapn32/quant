9457aa21fbe86a32e2cbb4e018c24e1145e0d782
Lars Eggert <lars@eggert.org>
2019-06-13 14:12:40 +0300

    More progress on particle build

--- particle/firmware/build.mk
+++ particle/firmware/build.mk
@@ -1,28 +1,51 @@
 INCLUDE_DIRS+=$(SOURCE_PATH)
 CPPSRC+=main.cpp
 
-DEPS=deps
+DEPS=lib/deps
 KLIB=$(DEPS)/klib
 PICOQUIC=$(DEPS)/picoquic
 PICOTLS=$(DEPS)/picotls
 WARPCORE=$(DEPS)/warpcore/lib
 
 INCLUDE_DIRS+=\
+	$(SOURCE_PATH)/lib/include \
 	$(SOURCE_PATH)/$(WARPCORE)/include \
 	$(SOURCE_PATH)/$(WARPCORE)/deps/klib
 
-CSRC+=\
+WARP_SRC+=\
 	warpcore/config.c \
 	$(WARPCORE)/src/backend_sock.c \
 	$(WARPCORE)/src/plat.c \
 	$(WARPCORE)/src/util.c \
 	$(WARPCORE)/src/warpcore.c
 
+QUANT_SRC+=\
+	quant/config.c \
+	lib/src/quic.c
+
+
+CSRC+=$(WARP_SRC) $(QUANT_SRC)
+
 EXTRA_CFLAGS+=-Wno-unknown-pragmas -Werror
 
 # TODO: figure out how to do this using make rules
-WARPCORE_VERSION:=$(shell grep 'warpcore.*VERSION' $(SOURCE_PATH)/../../lib/$(WARPCORE)/../CMakeLists.txt | cut -d' ' -f3)
-$(shell	cd $(SOURCE_PATH) && ln -sf ../../lib/deps)
+$(shell	cd $(SOURCE_PATH) && ln -sf ../../lib)
+
+QUANT_VERSION:=$(shell grep 'warpcore.*VERSION' $(SOURCE_PATH)/../../$(WARPCORE)/CMakeLists.txt | cut -d' ' -f3)
 $(shell	mkdir -p $(SOURCE_PATH)/warpcore)
-$(shell [ -s $(SOURCE_PATH)/warpcore/config.c ] || sed -E -e 's|@PROJECT_NAME@|warpcore|g; s|@PROJECT_NAME_UC@|WARPCORE|g; s|@PROJECT_VERSION@|$(WARPCORE_VERSION)|g; s|(#cmakedefine)|// \1|g' $(SOURCE_PATH)/../../lib/$(WARPCORE)/src/config.c.in > $(SOURCE_PATH)/warpcore/config.c)
-$(shell [ -s $(SOURCE_PATH)/warpcore/config.h ] || sed -E -e 's|@PROJECT_NAME@|warpcore|g; s|@PROJECT_NAME_UC@|WARPCORE|g; s|@PROJECT_VERSION@|$(WARPCORE_VERSION)|g; s|(#cmakedefine)|// \1|g' $(SOURCE_PATH)/../../lib/$(WARPCORE)/include/warpcore/config.h.in > $(SOURCE_PATH)/warpcore/config.h)
+$(shell [ -s $(SOURCE_PATH)/warpcore/config.c ] || \
+	sed -E -e 's|@PROJECT_NAME@|warpcore|g; s|@PROJECT_NAME_UC@|WARPCORE|g; s|@PROJECT_VERSION@|$(WARPCORE_VERSION)|g;' \
+		$(SOURCE_PATH)/../../$(WARPCORE)/src/config.c.in > $(SOURCE_PATH)/warpcore/config.c)
+$(shell [ -s $(SOURCE_PATH)/warpcore/config.h ] || \
+	sed -E -e 's|@PROJECT_NAME@|warpcore|g; s|@PROJECT_NAME_UC@|WARPCORE|g; s|@PROJECT_VERSION@|$(WARPCORE_VERSION)|g; s|(#cmakedefine)|// \1|g; s|(@.*@)|0|g;' \
+		$(SOURCE_PATH)/../../$(WARPCORE)/include/warpcore/config.h.in > $(SOURCE_PATH)/warpcore/config.h)
+
+QUANT_VERSION:=$(shell grep 'quant.*VERSION' $(SOURCE_PATH)/../../CMakeLists.txt | cut -d' ' -f3)
+DRAFT_VERSION:=$(shell grep 'quant.*VERSION' $(SOURCE_PATH)/../../CMakeLists.txt | cut -d' ' -f3 | cut -d. -f3)
+$(shell	mkdir -p $(SOURCE_PATH)/quant)
+$(shell [ -s $(SOURCE_PATH)/../lib/src/config.c ] || \
+	sed -E -e 's|@PROJECT_NAME@|quant|g; s|@PROJECT_NAME_UC@|QUANT|g; s|@PROJECT_VERSION@|$(QUANT_VERSION)|g; s|@PROJECT_VERSION_PATCH@|$(DRAFT_VERSION)|g; s|(@.*@)|0|g;' \
+		$(SOURCE_PATH)/../../lib/src/config.c.in > $(SOURCE_PATH)/quant/config.c)
+$(shell [ -s $(SOURCE_PATH)/../lib/include/quant/config.h ] || \
+	sed -E -e 's|@PROJECT_NAME@|quant|g; s|@PROJECT_NAME_UC@|QUANT|g; s|@PROJECT_VERSION@|$(QUANT_VERSION)|g; s|@PROJECT_VERSION_PATCH@|$(DRAFT_VERSION)|g; s|(#cmakedefine)|// \1|g; s|(@.*@)|0|g;' \
+		$(SOURCE_PATH)/../../lib/include/quant/config.h.in > $(SOURCE_PATH)/quant/config.h)
--- particle/firmware/main.cpp
+++ particle/firmware/main.cpp
@@ -4,8 +4,10 @@
 #include "warpcore/warpcore.h"
 
 
-static SerialDebugOutput log;
 static const int led = D7;
+static const float volt_div = 0.0011224;
+
+static SerialDebugOutput log;
 static int led_mode = HIGH;
 
 static struct w_engine * w;
@@ -22,7 +24,7 @@ static struct w_iov_sq o = w_iov_sq_initializer(o);
 void setup()
 {
     pinMode(led, OUTPUT);
-    w = w_init("wl3", 0, 50);
+    w = w_init("wl3", 0, 10);
     s = w_bind(w, 0, 0);
 
     static struct addrinfo hints;
@@ -41,9 +43,16 @@ void setup()
 void loop()
 {
     struct w_iov * const v = sq_first(&o);
-    const float voltage = analogRead(BATT) * 0.0011224;
-    v->len =
-        sprintf((char *)v->buf, "Hello from Particle! Voltage is %f.", voltage);
+    const float voltage = analogRead(BATT) * volt_div;
+
+    runtime_info_t info = {0};
+    info.size = sizeof(info);
+    HAL_Core_Runtime_Info(&info, 0);
+    v->len = sprintf((char *)v->buf,
+                     "Hello from Particle! OS %s, %.2fV, %" PRIu32 "/%" PRIu32
+                     "KB RAM",
+                     System.version().c_str(), voltage, info.freeheap / 0x400,
+                     info.total_init_heap / 0x400);
 
     digitalWrite(led, led_mode);
     led_mode = led_mode == HIGH ? LOW : HIGH;
@@ -51,12 +60,12 @@ void loop()
     w_tx(s, &o);
     while (w_tx_pending(&o))
         w_nic_tx(w);
-    warn(DBG, "pkt tx");
+    warn(DBG, "TX: %s", v->buf);
 
     struct w_iov_sq i = w_iov_sq_initializer(i);
     if (w_nic_rx(w, 0)) {
         w_rx(s, &i);
-        warn(DBG, "pkt rx");
+        warn(DBG, "RX");
     }
 
     delay(5000);
