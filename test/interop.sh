0f2d4df187e0d8ee9a6b8677c7b792b5c91bffc9
Lars Eggert <lars@eggert.org>
2019-10-10 16:09:34 +0300

    Various qns-related changes

--- test/interop.sh
+++ test/interop.sh
@@ -1,14 +1,20 @@
-#! /usr/bin/env bash
+#! /usr/bin/env sh
 set -e
 
 echo "Starting quant $ROLE..."
 echo "Params: $CLIENT_PARAMS"
 echo "Testcase: $TESTCASE"
 
-if [ "$ROLE" == "client" ]; then
+if [ "$TESTCASE" != "handshake" ]; then
+        exit 127
+fi
+
+if [ "$ROLE" = "client" ]; then
         cd /downloads
         /bin/client -i eth0 -w $REQUESTS
 else
         /bin/server -i eth0 -d /www -p 443 -t 0 \
                 -c /tls/dummy.crt -k /tls/dummy.key
 fi
+
+exit 0
