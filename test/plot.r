eb9fb9a1892d6e3e98422eb4744a3328afc2d399
Lars Eggert <lars@eggert.org>
2019-11-20 20:55:35 +0200

    Better plotting

--- test/plot.r
+++ test/plot.r
@@ -1,7 +1,44 @@
 #! /usr/bin/env Rscript
 
 library("tidyverse")
-library("cowplot")
+library("formattable")
+library("RColorBrewer")
+
+
+theme_lars = function(base_size = 9,
+                      base_family = "Times",
+                      base_line_size = base_size,
+                      base_rect_size = base_size) {
+    theme_void(base_size = base_size,
+               base_family = base_family,
+               base_line_size = base_line_size) %+replace%
+    theme(complete = TRUE,
+          line=element_line(color="black",
+                            size=1/2.835,
+                            linetype="solid",
+                            lineend="round"),
+          plot.title=element_text(size=base_size),
+          legend.text=element_text(size=base_size),
+          axis.title=element_text(size=base_size),
+          axis.title.y=element_text(angle=90,
+                                    margin=margin(0, base_size/2, 0, 0, "pt")),
+          axis.title.x=element_text(margin=margin(base_size/2, 0, 0, 0, "pt")),
+          axis.text=element_text(size=base_size),
+          axis.ticks.length=unit(1, "mm"),
+          panel.grid.major.x=element_blank(),
+          panel.grid.minor=element_blank(),
+          panel.grid.major.y=element_line(color="gray",
+                                          size=1/2.835,
+                                          linetype="solid",
+                                          lineend="round"),
+          legend.margin=margin(0, 0, 0, base_size/2, "pt"),
+          legend.key.size=unit(base_line_size, "pt")
+          )
+}
+
+update_geom_defaults("text", list(family=theme_lars()$text$family,
+                                  size=theme_lars()$text$size/2.835))
+
 
 # order matters!
 file_cat = list("app"           = "/firmware/main.cpp",
@@ -112,7 +149,7 @@ cat_by_func = function(func) {
                 i = which.min(is.na(m))
                 if (!is.na(m[i])) return(c)
         }
-        return("unknown")
+        return("misc")
 }
 
 
@@ -121,7 +158,8 @@ read_data = function(fname) {
              col_names=c("size", "type", "func", "file"),
              col_types="-cccc")
 
-        d = d %>% mutate(build=fname)
+        fields = str_split(fname, "[-.]")[[1]]
+        d = d %>% mutate(buildnr=strtoi(fields[1], base=10), build=fields[2])
         d = d %>% mutate(size=strtoi(size, base=16))
         d = d %>% mutate(file_mod=sapply(file, cat_by_file))
         d = d %>% mutate(func_mod=sapply(func, cat_by_func))
@@ -130,21 +168,58 @@ read_data = function(fname) {
 }
 
 
+ylabeller = function(b) {
+    dlab = d %>% distinct(build, buildnr)
+    return(dlab$build)
+}
+
+
+ybreaks = function(b) {
+    dlab = d %>% distinct(build, buildnr)
+    return(dlab$buildnr)
+}
+
+
+shortb = function(byte) {
+  ifelse(byte >= 10^9, paste(comma(format="d", byte/10^9), "G"),
+         ifelse(byte >= 10^6, paste(comma(format="d", byte/10^6), "M"),
+                ifelse(byte >= 10^3, paste(comma(format="d", byte/10^3), "K"),
+                       comma(format="d", byte))))
+}
+
+
 d = bind_rows(lapply(commandArgs(trailingOnly=TRUE), read_data))
 
-# options(width = 10000)
-# print(d)
+components = list("quic" = c("quant", "warpcore", "timeout"),
+                  "tls"  = c("picotls", "cifra", "micro-ecc"),
+                  "glue" = c("libc", "os", "misc")
+                 )
 
-dsum = d %>% group_by(build, module) %>% summarise(sum=sum(size))
-# print(dsum,n=100)
+order = c("app", unlist(components, use.names=FALSE))
 
-ggsave2("argon.pdf", device="pdf", units="in", width=7, height=3.5,
+dsum = d %>% group_by(buildnr, module) %>% summarise(sum=sum(size))
+dsum$module = ordered(dsum$module, order)
+
+dsum = dsum %>% mutate(component=case_when(module %in% components$quic ~ "quic",
+                                           module %in% components$tls ~ "tls",
+                                           module == "app" ~ "app",
+                                           TRUE ~ "glue"))
+print(dsum)
+
+pal=c(rev(brewer.pal(name="Purples", n=4))[c(1)],
+      rev(brewer.pal(name="Reds", n=4))[c(1, 2, 3)],
+      rev(brewer.pal(name="Greens", n=4))[c(1, 2, 3)],
+      rev(brewer.pal(name="Blues", n=4))[c(1, 2, 3)])
+
+ggsave("argon.pdf", device=cairo_pdf, units="in", width=7, height=3.5,
        plot=ggplot() +
-        geom_col(aes(y=sum, x=build, fill=module), data=dsum) +
-        theme_cowplot(font_size=9, font_family="Times") +
-        theme(axis.text.x=element_text(angle=-60)) +
-        background_grid(major="y", minor="none"))
-
-
+       geom_col(data=dsum, aes(y=sum, x=buildnr, fill=module)) +
+       # geom_text(data=dsum, aes(y=sum, x=buildnr, label=sum)) +
+       scale_x_continuous(labels=ylabeller, breaks=ybreaks,
+                          name="Build options", expand=c(0, 0)) +
+       scale_y_continuous(labels=shortb, name="Size", expand=c(0, 0)) +
+       scale_fill_manual(values = pal) +
+       theme_lars() +
+       theme(axis.text.x=element_text(angle=-30, hjust=0)))
 
 
