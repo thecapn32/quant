3075dfb2a355b43c05b174f511754e93eaf27589
Lars Eggert <lars@eggert.org>
2019-11-27 15:20:56 +0200

    Use fpvgcc

--- test/build-matrix.sh
+++ test/build-matrix.sh
@@ -1,26 +1,50 @@
 #! /usr/bin/env bash
 
+if [ "$1" != particle ] && [ "$1" != riot ]; then
+    echo particle or riot
+    exit 1
+fi
+
+set -e
 
 opts=('' NO_64BIT NO_SERVER MINIMAL_CIPHERS NO_MIGRATION NO_ERR_REASONS
       NO_SRT_MATCHING NO_OOO_0RTT NO_OOO_DATA NO_QINFO)
 
-always="-DNDEBUG -DRELEASE_BUILD"
+declare -A always=([particle]="-DNDEBUG -DRELEASE_BUILD"
+                   [riot]="-DNDEBUG")
+
+declare -A nm=([particle]=arm-none-eabi-nm
+               [riot]=xtensa-esp32-elf-nm)
+
+declare -A pref=([particle]=bin/particle-argon
+                 [riot]=bin/esp32-wroom-32/quant-client)
 
 i=0
 for flag in "${opts[@]}"; do
         b=$((!i))
-        ((i++))
-        data="$i-${flag:-NONE}".func
+        ((i++)) || true
+        data="$i-${flag:-NONE}"
         flags="${flag:+-D$flag} $flags"
-        if [ ! -s "$data" ]; then
+        if [ ! -s "$data".elf ]; then
                 echo -n "${flag:-NONE}"
-                out=$(env COMPILE_LTO=y \
-                        BUILD_FLAGS="$always -DHAVE_64BIT=$b $flags" \
-                        po argon build 2>/dev/null | grep particle-argon.elf)
-                bin=$(echo $out | cut -d' ' -f6)
-                echo "$out"
-                arm-none-eabi-nm -C -l -S --size-sort "$bin" | \
+                all="${always[$1]} -DHAVE_64BIT=$b $flags"
+                case $1 in
+                    particle)
+                        out=$(env MODULAR=n COMPILE_LTO=n BUILD_FLAGS="$all" \
+                                po argon build 2>/dev/null | \
+                                    grep particle-argon.elf)
+                        ;;
+                    riot)
+                        out=$(make LTO=0 BUILD_IN_DOCKER=1 DEVELHELP=0 \
+                             BUILD_FLAGS=1 \
+                             DOCKER_ENVIRONMENT_CMDLINE="-e BUILD_FLAGS=\"$all\"")
+                        ;;
+                esac
+
+                echo "$out" | tail -n 1
+                ${nm[$1]} -C -l -S --size-sort "${pref[$1]}".elf | \
                         gsed -e 's| |\t|' -e 's| |\t|' -e 's| |\t|' | \
-                                cut -f2- > "$data"
+                                cut -f2- > "$data".elf
+                fpvgcc --sobj all "${pref[$1]}".map > "$data".map
         fi
 done
