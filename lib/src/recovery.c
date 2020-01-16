419e3af0af7964aeec13d0bc9777a5e7f480561e
Lars Eggert <lars@eggert.org>
2020-01-16 09:53:16 +0200

    Fix

--- lib/src/recovery.c
+++ lib/src/recovery.c
@@ -640,7 +640,7 @@ void on_pkt_acked(struct w_iov * const v, struct pkt_meta * m)
                  ((m->hdr.nr != UINT_T_MAX && m->hdr.type == SH) ||
                   (m->hdr.nr == c->pmtud_pkt_nr && m->hdr.type == LH_HSHK)))) {
         c->rec.max_pkt_size =
-            MIN(w_max_udp_payload(ws), (uint16_t)c->tp_out.max_pkt);
+            MIN(w_max_udp_payload(c->sock), (uint16_t)c->tp_out.max_pkt);
         warn(NTE, "PMTU %u validated", c->rec.max_pkt_size);
         c->pmtud_pkt_nr = UINT_T_MAX;
     }
