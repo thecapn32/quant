50d0e55c59a21a87db048f8a4bda5a2be80c7717
Lars Eggert <lars@eggert.org>
2020-02-18 09:18:53 +0200

    Particle doesn't have printf, don't log qinfo for release builds

--- lib/src/quic.c
+++ lib/src/quic.c
@@ -750,7 +750,7 @@ void q_close(struct q_conn * const c,
     loop_run(c->w, (func_ptr)q_close, c, 0);
 
 done:
-#if !defined(NO_QINFO)
+#if !defined(NO_QINFO) && !defined(PARTICLE)
     if (c->scid && c->i.pkts_in_valid > 0) {
         static const char * const frm_typ_str[] = {
             [0x00] = "PADDING",
