adb2c6c741eae522cd4fc08c60c8036dadf6bcf0
Lars Eggert <lars@eggert.org>
2020-04-28 21:17:19 +0300

    Fix CI

--- test/test_hex2str.c
+++ test/test_hex2str.c
@@ -36,7 +36,6 @@
 #pragma clang diagnostic ignored "-Wcast-qual"
 #pragma clang diagnostic ignored "-Wundef"
 #pragma clang diagnostic ignored "-Wimplicit-function-declaration"
-#define WITH_OPENSSL
 #include "quic.h"
 #include "tls.h"
 #pragma clang diagnostic pop
