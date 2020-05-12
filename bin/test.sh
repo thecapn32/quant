0a5ac08887fae1b7aafecefd4b76db75b15ed203
Lars Eggert <lars@eggert.org>
2020-05-12 20:15:48 +0300

    Add path to quic-tracker run

--- bin/test.sh
+++ bin/test.sh
@@ -76,7 +76,7 @@ case $c in
                         CGO_CFLAGS=-I/usr/local/opt/openssl@1.1/include \
                         CGO_LDFLAGS=-L/usr/local/opt/openssl@1.1/lib \
                     go run $(pwd)/external/go/src/github.com/QUIC-Tracker/quic-tracker/bin/test_suite/scenario_runner.go \
-                        -interface lo0 -host $addr:$port -scenario http_get_on_uni_stream"
+                        -interface lo0 -host $addr:$port -path $path -scenario http_get_on_uni_stream"
                 ;;
         quic-go)
                 cc="env GOPATH=$(pwd)/external/go go run \
